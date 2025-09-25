a6149069844c6924e464b8d3f4c5f090bf13bed4
daniel-lopes-codbr <daniel.lopes.codbr@gmail.com>
2025-09-25 18:48:50 -0300

    Update Program.cs

--- src/FCG_MS_Users.Api/Program.cs
+++ src/FCG_MS_Users.Api/Program.cs
@@ -65,11 +65,8 @@ builder.Services.AddHttpClient<IUserNotificationClient, UserNotificationClient>(
 });
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 using (var scope = app.Services.CreateScope())
 {
